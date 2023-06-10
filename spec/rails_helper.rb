08b5ad827915bd4a298c2787802c6d1ae42032e7
Thomas Hawley <thawley22@turing.edu>
2023-06-10 12:33:07 -0600

    update: Add weather api key to vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.filter_sensitive_data('<mapquest_api_key>') { ENV['MAPQUEST_API_KEY'] }
+  config.filter_sensitive_data('<open_weather_api_key>') { ENV['WEATHER_API_KEY'] }
   config.configure_rspec_metadata!
 end
 
