351b3f8d65ec75f2812acae2ad76d984259831ed
Thomas Hawley <thawley22@turing.edu>
2023-06-10 15:27:56 -0600

    refactor: Add default id of nil

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -1,7 +1,8 @@
 class Weather
-  attr_reader :current_weather, :daily_weather, :hourly_weather
+  attr_reader :current_weather, :daily_weather, :hourly_weather, :id
 
   def initialize(data)
+    @id = nil
     @current_weather = data[:current_weather]
     @daily_weather = data[:daily_weather]
     @hourly_weather = data[:hourly_weather]
