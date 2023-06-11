fd714657fcadb72634a76bc1f56338ca1b1764dc
Thomas Hawley <thawley22@turing.edu>
2023-06-11 15:31:46 -0600

    test: Add test for get_directions service method

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -14,5 +14,26 @@ RSpec.describe LocationService do
         expect(denver[:results][0][:locations][0][:latLng][:lng]).to be_a Float
       end
     end
+
+    it 'can return the directions between two locations' do
+      VCR.use_cassette('ponca_city_to_denver_location_service') do
+        pc_to_dv = LocationService.new.get_directions('ponca city, ok', 'denver, co')
+
+        expect(pc_to_dv).to be_a Hash
+        expect(pc_to_dv).to have_key :route
+        expect(pc_to_dv[:route]).to have_key :formattedTime
+        expect(pc_to_dv[:route][:formattedTime]).to be_a String
+        expect(pc_to_dv[:route][:locations][0][:latLng]).to have_key :lat
+        expect(pc_to_dv[:route][:locations][0][:latLng][:lat]).to be_a Float
+        expect(pc_to_dv[:route][:locations][0][:latLng]).to have_key :lng
+        expect(pc_to_dv[:route][:locations][0][:latLng][:lng]).to be_a Float
+        expect(pc_to_dv[:route][:locations][1][:latLng]).to have_key :lat
+        expect(pc_to_dv[:route][:locations][1][:latLng][:lat]).to be_a Float
+        expect(pc_to_dv[:route][:locations][1][:latLng]).to have_key :lng
+        expect(pc_to_dv[:route][:locations][1][:latLng][:lng]).to be_a Float
+
+        
+      end
+    end
   end
 end
