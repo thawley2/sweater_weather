80d359f15f1969eb5511f4c601dc569a3f2ec519
Thomas Hawley <thawley22@turing.edu>
2023-06-11 10:03:33 -0600

    Add Faker to name in email

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.email(name: name, separators: ['-']) }
+    email { Faker::Internet.email(name: Faker::Name.name, separators: ['-']) }
     password { Faker::Internet.password }
     api_key { SecureRandom.hex(13) }
   end
