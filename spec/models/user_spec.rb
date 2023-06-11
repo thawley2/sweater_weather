f535c7d663bbb55e2ae5a0b693dcc81a060fb1df
Thomas Hawley <thawley22@turing.edu>
2023-06-11 14:37:56 -0600

    test: Add test for case insensitive email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
-  it { should validate_uniqueness_of :email }
+  it { should validate_uniqueness_of(:email).case_insensitive }
   it { should have_secure_password }
 
   describe 'before_create' do
