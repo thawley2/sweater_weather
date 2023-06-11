115a6241ac3cb7b065468b95868b7b63bc9f88cf
Thomas Hawley <thawley22@turing.edu>
2023-06-11 13:51:56 -0600

    refactor: Abstract the api key creation to the model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::UsersController < ApplicationController
   def create
     if params[:password] == params[:password_confirmation]
       user = User.create!(user_params)
-      user.update(api_key: SecureRandom.hex(13))
       render json: UsersSerializer.new(user), status: 201
     else
       render json: ErrorSerializer.serialize("Password and password confirmation do not match"), status: 400
