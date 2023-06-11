29dc9eedad6f606ec7c700cca1fa07643c03ef15
Thomas Hawley <thawley22@turing.edu>
2023-06-11 00:46:09 -0600

    refactor: Move the error handling to the application controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,12 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    begin
-      if params[:password] == params[:password_confirmation]
-        user = User.create!(user_params)
-        user.update(api_key: SecureRandom.hex(13))
-        render json: UsersSerializer.new(user), status: 201
-      else
-        render json: ErrorSerializer.serialize("Password and password confirmation do not match"), status: 400
-      end
-      rescue ActiveRecord::RecordInvalid => error
-        render json: ErrorSerializer.serialize(error), status: 422
+    if params[:password] == params[:password_confirmation]
+      user = User.create!(user_params)
+      user.update(api_key: SecureRandom.hex(13))
+      render json: UsersSerializer.new(user), status: 201
+    else
+      render json: ErrorSerializer.serialize("Password and password confirmation do not match"), status: 400
     end
   end
 
