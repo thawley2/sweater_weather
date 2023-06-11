097c9fb35932c5c9798ed1ac9dd6ef8603526330
Thomas Hawley <thawley22@turing.edu>
2023-06-11 00:45:52 -0600

    refactor: Move error handling to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: :error_message
+
+  def error_message(error)
+    if error.message.include?("Email can't be blank")
+      render json: ErrorSerializer.serialize(error.message), status: 422
+    else
+      render json: ErrorSerializer.serialize(error.message), status: 409
+    end
+  end
 end
