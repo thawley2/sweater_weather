4788b6cf75880b1c4263d931256f9f02ba732e35
Thomas Hawley <thawley22@turing.edu>
2023-06-11 13:52:43 -0600

    refactor: Move api key creation to the user model before_create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  before_create :set_api_key
   validates :email, presence: true, uniqueness: true
 
   has_secure_password
+
+  def set_api_key
+    self.api_key = SecureRandom.hex(13)
+  end
 end
