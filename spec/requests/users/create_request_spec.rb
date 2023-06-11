9134805e82a78769d1972ea9314c46944c3b64cc
Thomas Hawley <thawley22@turing.edu>
2023-06-11 00:45:27 -0600

    test: Add tests for sad path error handling

--- spec/requests/users/create_request_spec.rb
+++ spec/requests/users/create_request_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe 'Users API' do
       expect(user_data[:data][:attributes]).to_not have_key :password_digest
     end
 
-    it 'returns an error if email is already taken status 422' do
+    it 'returns an error if email is already taken status 409' do
       User.create!(email: "whatever@example.com", password: "password", api_key: "lfdkgj34t98sd34dfhj")
       user_params = {
         email: "whatever@example.com",
@@ -42,6 +42,51 @@ RSpec.describe 'Users API' do
         password_confirmation: "password"
       }
 
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(409)
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error).to be_a Hash
+      expect(error).to have_key :errors
+      expect(error[:errors]).to be_a Array
+      expect(error[:errors][0]).to be_a Hash
+      expect(error[:errors][0]).to have_key :detail
+      expect(error[:errors][0][:detail]).to eq("Validation failed: Email has already been taken")
+    end
+
+    it 'returns an error if the password and confirmation_password do not match status 400' do
+      user_params = {
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "not_the_same"
+      }
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error).to be_a Hash
+      expect(error).to have_key :errors
+      expect(error[:errors]).to be_a Array
+      expect(error[:errors][0]).to be_a Hash
+      expect(error[:errors][0]).to have_key :detail
+      expect(error[:errors][0][:detail]).to eq("Password and password confirmation do not match")
+    end
+
+    it 'returns an error if a field is missing, status 422' do
+      user_params = {
+        email: "",
+        password: "password",
+        password_confirmation: "password"
+      }
       headers = {"CONTENT_TYPE" => "application/json"}
       post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
@@ -55,11 +100,7 @@ RSpec.describe 'Users API' do
       expect(error[:errors]).to be_a Array
       expect(error[:errors][0]).to be_a Hash
       expect(error[:errors][0]).to have_key :detail
-      expect(error[:errors][0][:detail]).to eq("Validation failed: Email has already been taken")
-    end
-
-    xit 'returns an error if the password and confirmation_password do not match status 400' do
-      
+      expect(error[:errors][0][:detail]).to eq("Validation failed: Email can't be blank")
     end
   end
 end
