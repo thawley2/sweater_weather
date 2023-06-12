97c6d30eef4e7e6f4704d6b0b851ecdc979544f9
Thomas Hawley <thawley22@turing.edu>
2023-06-12 10:03:54 -0600

    test: Add test for books by location poro

---
+++ spec/poros/books_by_location_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe BooksByLocation do
+  it 'exists and has attributes' do
+    forecast_data = {
+      destination: 'denver, co',
+      forecast: {
+        summary: 'cloudy',
+        temperature: 50.0
+      }
+    }
+
+    books_data = {
+      total_books_found: 700,
+      books: [
+        {
+          isbn: ['123456', '465789'],
+          title: 'Denver, the best city ever',
+          publisher: ['Denver Publishing'],
+        },
+        {
+          isbn: ['468765', '897325135'],
+          title: 'Denver Electrical Grids',
+          publisher: ['Electricity Publishing'],
+        }
+      ]
+    }
+
+    books_with_weather = BooksByLocation.new(forecast_data, books_data)
+
+    expect(books_with_weather).to be_a BooksByLocation
+    expect(books_with_weather.destination).to eq('denver, co')
+    expect(books_with_weather.forecast).to be_a Hash
+    expect(books_with_weather.forecast).to have_key :summary
+    expect(books_with_weather.forecast[:summary]).to eq('cloudy')
+    expect(books_with_weather.forecast).to have_key :temperature
+    expect(books_with_weather.forecast[:temperature]).to eq(50.0)
+    expect(books_with_weather.total_books_found).to eq(700)
+    expect(books_with_weather.books).to be_an Array
+    expect(books_with_weather.books.count).to eq(2)
+    expect(books_with_weather.books.first).to be_a Hash
+    expect(books_with_weather.books.first).to have_key :isbn
+    expect(books_with_weather.books.first[:isbn]).to be_an Array
+    expect(books_with_weather.books.first[:isbn].count).to eq(2)
+    expect(books_with_weather.books.first[:isbn].first).to eq('123456')
+    expect(books_with_weather.books.first[:isbn].last).to eq('465789')
+    expect(books_with_weather.books.first).to have_key :title
+    expect(books_with_weather.books.first[:title]).to eq('Denver, the best city ever')
+    expect(books_with_weather.books.first).to have_key :publisher
+    expect(books_with_weather.books.first[:publisher]).to be_an Array
+    expect(books_with_weather.books.first[:publisher].count).to eq(1)
+    expect(books_with_weather.books.first[:publisher].first).to eq('Denver Publishing')
+  end
+end
