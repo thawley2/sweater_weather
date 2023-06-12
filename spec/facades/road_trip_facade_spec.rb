281aa46a97e49ecb6f7961ec8ad764ce4b74ea7d
Thomas Hawley <thawley22@turing.edu>
2023-06-12 16:57:38 -0600

    test: Add sad path for road trip facade if a trip is impossible

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -26,5 +26,26 @@ RSpec.describe RoadTripFacade do
         end
       end
     end
+
+    it 'returns an object with an impossible travel time if the trip is impossible' do
+      VCR.use_cassette('trip_impossible', allow_playback_repeats: true) do
+        frozen_time = Time.new(2023, 6, 12, 12, 0, 0, '+00:00')
+        Timecop.freeze(frozen_time) do
+
+          trip_data = {
+            origin: 'ponca city, ok',
+            destination: 'london, uk'
+          }
+
+          bad_trip = RoadTripFacade.new(trip_data).road_trip
+
+          expect(bad_trip).to be_a RoadTrip
+          expect(bad_trip.start_city).to eq('ponca city, ok')
+          expect(bad_trip.end_city).to eq('london, uk')
+          expect(bad_trip.travel_time).to eq('impossible')
+          expect(bad_trip.weather_at_eta).to eq({})
+        end
+      end
+    end
   end
 end
