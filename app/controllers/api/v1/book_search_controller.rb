77e99dae14763aa990791e3e8811e171128e7440
Thomas Hawley <thawley22@turing.edu>
2023-06-12 10:53:31 -0600

    refactor: Update index action with error handling

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-    render json: BooksSerializer.new(WeatherFacade.new(params[:location], params[:quantity]).books_by_location)
+    if params[:location].present? && params[:quantity].to_i > 1
+      render json: BooksSerializer.new(WeatherFacade.new(params[:location], params[:quantity]).books_by_location)
+    else
+      render json: ErrorSerializer.serialize("Invalid location or quantity"), status: 400
+    end
   end
 end
