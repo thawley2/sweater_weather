d04e6ee044c23433cf3fe3ee2d7a5314e1fd34d9
Thomas Hawley <thawley22@turing.edu>
2023-06-11 22:00:02 -0600

    Setup timezone gem initializer

---
+++ config/initializers/timezone.rb
@@ -0,0 +1,7 @@
+# Timezone::Lookup.config(:geonames) do |c|
+#   c.username = 'tofuzzy70'
+# end
+
+Timezone::Lookup.config(:google) do |c|
+  c.api_key = 'AIzaSyBinyjDyO9fMx6hAUIqaV77lDjjF8lYuQI'
+end
