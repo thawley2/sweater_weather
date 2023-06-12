d17aa4c5291d947c1c4617fbeeb7ffb811cfe7d6
Thomas Hawley <thawley22@turing.edu>
2023-06-12 12:00:16 -0600

    refactor: Update if logic in controller

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-    if params[:location].present? && params[:quantity].to_i > 1
+    if params[:location].present? && params[:quantity].to_i >= 1
       render json: BooksSerializer.new(WeatherFacade.new(params[:location], params[:quantity]).books_by_location)
     else
       render json: ErrorSerializer.serialize("Invalid location or quantity"), status: 400
