643f7e249daa7e8b3e2610a2484a10ed22f405af
Thomas Hawley <thawley22@turing.edu>
2023-06-12 18:45:48 -0600

    feat: Add logic to create action to check validity of the api key received

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
+    if User.find_by(api_key: params[:api_key])
     render json: RoadTripSerializer.new(RoadTripFacade.new(road_trip_params).road_trip)
+    else
+    render json: ErrorSerializer.serialize("API key is invalid"), status: 401
+    end
   end
 
   private
